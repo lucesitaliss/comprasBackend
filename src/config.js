8e945971e4997bd2b9084aa4b5fe314193553452
lissette <ing.liss87@gmail.com>
2022-12-08 12:11:02 -0300

    kjdkjg

--- src/config.js
+++ src/config.js
@@ -11,27 +11,26 @@ config()
 //   },
 // }
 
-module.exports = {
-  PORT: process.env.PORT || 4001,
-  db: {
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || '2020',
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || '5432',
-    database: process.env.DB_DATABASE || 'cart',
-  },
-}
-
-//postgres:8Ns1Vp8y4CZ181rPiP7D@containers-us-west-94.railway.app:7338/railway
-
-//PGPASSWORD=0UsnvNChzV5ONseF7V1z psql -h containers-us-west-141.railway.app -U postgres -p 6408 -d railway
 // module.exports = {
 //   PORT: process.env.PORT || 4001,
 //   db: {
-//     user: 'postgres',
-//     password: '0UsnvNChzV5ONseF7V1z' || '2020',
-//     host: 'containers-us-west-141.railway.app' || 'localhost',
-//     port: '6408' || '5432',
-//     database: 'railway' || 'cart',
+//     user: process.env.DB_USER || 'postgres',
+//     password: process.env.DB_PASSWORD || '2020',
+//     host: process.env.DB_HOST || 'localhost',
+//     port: process.env.DB_PORT || '5432',
+//     database: process.env.DB_DATABASE || 'cart',
 //   },
 // }
+
+// postgres:Od4ya334jz6Ewka5cCpt@containers-us-west-153.railway.app:6481/railway
+
+module.exports = {
+  PORT: process.env.PORT || 4001,
+  db: {
+    user: 'postgres',
+    password: 'Od4ya334jz6Ewka5cCpt' || '2020',
+    host: 'containers-us-west-153.railway.app' || 'localhost',
+    port: '6481' || '5432',
+    database: 'railway' || 'cart',
+  },
+}
