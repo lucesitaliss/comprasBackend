26ccbfa6918b6139640665e812b85118304553db
lissette <ing.liss87@gmail.com>
2022-12-08 08:58:40 -0300

    new configuration conetion bd

--- src/config.js
+++ src/config.js
@@ -10,13 +10,27 @@ config()
 //     database: process.env.DB_DATABASE,
 //   },
 // }
-module.exports = {
+
+// module.exports = {
+//   PORT: process.env.PORT || 4001,
+//   db: {
+//     user: process.env.DB_USER || 'postgres',
+//     password: process.env.DB_PASSWORD || '2020',
+//     host: process.env.DB_HOST || 'localhost',
+//     port: process.env.DB_PORT || '5432',
+//     database: process.env.DB_DATABASE || 'cart',
+//   },
+// }
+
+//postgres:gWUMwA3fPFgwD0Xnxh7K@containers-us-west-93.railway.app:7524/railway
+
+postgresql: module.exports = {
   PORT: process.env.PORT || 4001,
   db: {
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || '2020',
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || '5432',
-    database: process.env.DB_DATABASE || 'cart',
+    user: 'postgres',
+    password: 'gWUMwA3fPFgwD0Xnxh7K' || '2020',
+    host: 'containers-us-west-93.railway.app' || 'localhost',
+    port: '7524' || '5432',
+    database: 'railway' || 'cart',
   },
 }
