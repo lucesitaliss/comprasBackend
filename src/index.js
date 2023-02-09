5e84405b6f1e39c16f71d950241409ce6d51e764
lucesitaliss <ing.liss87@gmail.com>
2023-02-09 13:33:51 -0300

    ffdñlkñ

--- src/index.js
+++ src/index.js
@@ -1,72 +1,40 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const jwt = require('jsonwebtoken')
+// const cors = require('cors')
+// const jwt = require('jsonwebtoken')
 
-//Configuración del socket
-const { Server } = require('socket.io')
-const http = require('http')
-const server = http.createServer(app)
-// const io = new Server(server, {
-//   cors: {
-//     origin: '*',
-//   },
-// })
+// const morgan = require('morgan')
 
-const morgan = require('morgan')
+// const { PORT } = require('./config.js')
+// const errors = require('./utils/errors.js')
+// const session = require('express-session')
 
-const { PORT } = require('./config.js')
-const errors = require('./utils/errors.js')
-const session = require('express-session')
+// const categoriaRoutes = require('./routes/category.routes')
+// const productRoutes = require('./routes/productRoutes')
+// const cartRoutes = require('./routes/cartRoutes')
 
-const categoriaRoutes = require('./routes/category.routes')
-const productRoutes = require('./routes/productRoutes')
-const cartRoutes = require('./routes/cartRoutes')
+// const usersRouter = require('./routes/usersRouter')
 
-const usersRouter = require('./routes/usersRouter')
-
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.urlencoded({ extended: true }))
+// app.use(cors())
+// app.use(morgan('dev'))
+// app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// app.use(
-//   session({
-//     name: 'sid',
-//     secret: 'secret',
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       maxAge: 1000 * 60 * 60 * 2, // 2 horas la duración de la cookie
-//     },
-//   }),
-// )
+// app.use(categoriaRoutes)
+// app.use(productRoutes)
+// app.use(cartRoutes)
+// app.use(usersRouter)
 
-app.use(categoriaRoutes)
-app.use(productRoutes)
-app.use(cartRoutes)
-app.use(usersRouter)
-
-// io.on('connection', (socket) => {
-//   const isLoggedIn = req.session.loggedIn
-//   socket.emit('isLoggedIn', isLoggedIn)
-
+// app.use((error, req, res, next) => {
+//   console.error(error)
+//   if (errors[error.routine]) {
+//     return res
+//       .status(errors[error.routine].httpCode)
+//       .json({ error: errors[error.rountine].message })
+//   }
+//   return res.status(500).json(error.message)
 // })
-
-// io.on('connection', (socket) => {
-//   console.log('socket connet', socket.id)
-// })
-
-app.use((error, req, res, next) => {
-  console.error(error)
-  if (errors[error.routine]) {
-    return res
-      .status(errors[error.routine].httpCode)
-      .json({ error: errors[error.rountine].message })
-  }
-  return res.status(500).json(error.message)
-})
 
 // app.listen(PORT)
-server.listen(PORT)
+server.listen(3000)
 console.log(`el puerto ${PORT} es del servidor de backend`)
