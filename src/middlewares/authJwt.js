0fa71e9f4d2d59abcb6ae3c404681a37484ca443
lucesitaliss <ing.liss87@gmail.com>
2023-02-09 15:47:22 -0300

    ,jdsfpfkfeppppppp

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -3,18 +3,18 @@
 const jwt = require('jsonwebtoken')
 const pool = require('../db')
 
-// const verifyToken = async (req, res, next) => {
-//   const token = req.headers['x-acces-token']
-//   if (!token) return res.status(403).send('No token provided')
-//   const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-//   console.log(decoded)
-//   const dbUser = await pool.query('SELECT * FROM users WHERE password= $1', [
-//     decoded.password,
-//   ])
-//   if (!dbUser) return res.status(404).send('No user found')
+const verifyToken = async (req, res, next) => {
+  const token = req.headers['x-acces-token']
+  if (!token) return res.status(403).send('No token provided')
+  const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+  console.log(decoded)
+  const dbUser = await pool.query('SELECT * FROM users WHERE password= $1', [
+    decoded.password,
+  ])
+  if (!dbUser) return res.status(404).send('No user found')
   
-//   next()
-// }
+  next()
+}
 
 const verifyToken = async (req, res, next) => {
   next()
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -8,20 +8,13 @@ const {
   updateDeleteCategory,
   deleteCategory,
 } = require('../controllers/category.controller')
-// const { verifyToken } = require('../middlewares/authJwt')
+const { verifyToken } = require('../middlewares/authJwt')
 
-router.get('/categories', getCategories)
+router.get('/categories', verifyToken, getCategories)
 router.get('/category/:id', getCategoryById)
 router.post('/category', insertCategory)
-router.put('/category/', updateCategory)
-router.put('/category/delete/:id', updateDeleteCategory)
+router.put('/category/', verifyToken, updateCategory)
+router.put('/category/delete/:id', verifyToken, updateDeleteCategory)
 router.delete('/category/:id', deleteCategory)
-
-// router.get('/categories', verifyToken, getCategories)
-// router.get('/category/:id', getCategoryById)
-// router.post('/category', insertCategory)
-// router.put('/category/',verifyToken, updateCategory)
-// router.put('/category/delete/:id',verifyToken, updateDeleteCategory)
-// router.delete('/category/:id', deleteCategory)
 
 module.exports = router
