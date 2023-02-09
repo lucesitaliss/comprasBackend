2fd63e5249bdff8aa7bd6545671a97c88dad7136
lucesitaliss <ing.liss87@gmail.com>
2023-02-09 15:41:11 -0300

    ,jdsfpfkfeppppppp

--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
- const express = require('express')
- const app = express()
- const cors = require('cors')
- const jwt = require('jsonwebtoken')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
 
- const morgan = require('morgan')
+const morgan = require('morgan')
 
 // const { PORT } = require('./config.js')
- const errors = require('./utils/errors.js')
+const errors = require('./utils/errors.js')
 // const session = require('express-session')
 
-// const categoriaRoutes = require('./routes/category.routes')
-// const productRoutes = require('./routes/productRoutes')
-// const cartRoutes = require('./routes/cartRoutes')
+const categoriaRoutes = require('./routes/category.routes')
+const productRoutes = require('./routes/productRoutes')
+const cartRoutes = require('./routes/cartRoutes')
+const usersRouter = require('./routes/usersRouter')
 
-// const usersRouter = require('./routes/usersRouter')
-
-// app.use(cors())
-// app.use(morgan('dev'))
+app.use(cors())
+app.use(morgan('dev'))
 // app.use(express.urlencoded({ extended: true }))
-// app.use(express.json())
+app.use(express.json())
 
-// app.use(categoriaRoutes)
-// app.use(productRoutes)
-// app.use(cartRoutes)
-// app.use(usersRouter)
+app.use(categoriaRoutes)
+app.use(productRoutes)
+app.use(cartRoutes)
+app.use(usersRouter)
 
-// app.use((error, req, res, next) => {
-//   console.error(error)
-//   if (errors[error.routine]) {
-//     return res
-//       .status(errors[error.routine].httpCode)
-//       .json({ error: errors[error.rountine].message })
-//   }
-//   return res.status(500).json(error.message)
-// })
+app.use((error, req, res, next) => {
+  //   console.error(error)
+  //   if (errors[error.routine]) {
+  //     return res
+  //       .status(errors[error.routine].httpCode)
+  //       .json({ error: errors[error.rountine].message })
+  //   }
+  //   return res.status(500).json(error.message)
+})
 
- app.listen(3000)
+app.listen(3000)
 // //server.listen(3000)
 // console.log(`el puerto ${PORT} es del servidor de backend`)
 
