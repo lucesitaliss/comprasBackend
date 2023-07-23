307f8488307dac6477b602b5575295cc66b0f7da
larts85 <larts85@yahoo.com>
2023-07-23 18:12:54 -0300

    Add port to env.Add listen message. Add allowed orogins.

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -1,25 +1,26 @@
 // middlewares de autorizacion
-
-const jwt = require('jsonwebtoken')
-const pool = require('../db')
+const jwt = require("jsonwebtoken")
+const pool = require("../db")
 
 const verifyToken = async (req, res, next) => {
-  const token = req.headers['x-acces-token']
-  if (!token) return res.status(403).send('No token provided')
-  const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-  console.log(decoded)
-  const dbUser = await pool.query('SELECT * FROM users WHERE password= $1', [
-    decoded.password,
-  ])
-  if (!dbUser) return res.status(404).send('No user found')
+	try {
+		const token = req.headers["x-acces-token"]
+		if (!token) {
+			return res.status(403).send("No token provided")
+		}
 
-  next()
+		const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+		const dbUser = await pool.query("SELECT * FROM users WHERE password= $1", [
+			decoded.password,
+		])
+		if (!dbUser) return res.status(404).send("No user found")
+
+		next()
+	} catch (error) {
+		return res.status(401).send("Authentication failed")
+	}
 }
 
-// const verifyToken = async (req, res, next) => {
-//   next()
-// }
-
 module.exports = {
-  verifyToken,
+	verifyToken,
 }
