91c73d6885a974f5c1702b5db761f7703245239a
lucesitaliss <ing.liss87@gmail.com>
2023-08-17 11:57:00 -0300

    switch to server

--- src/config.js
+++ src/config.js
@@ -1,26 +1,26 @@
 const { config } = require('dotenv')
 config()
 
-module.exports = {
-  PORT: process.env.PORT || 4001,
-  db: {
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || '2020',
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || '5432',
-    database: process.env.DB_DATABASE || 'cart',
-  },
-}
+// module.exports = {
+//   PORT: process.env.PORT || 4001,
+//   db: {
+//     user: process.env.DB_USER || 'postgres',
+//     password: process.env.DB_PASSWORD || '2020',
+//     host: process.env.DB_HOST || 'localhost',
+//     port: process.env.DB_PORT || '5432',
+//     database: process.env.DB_DATABASE || 'cart',
+//   },
+// }
 
 // postgres:Od4ya334jz6Ewka5cCpt@containers-us-west-153.railway.app:6481/railway
 
-// module.exports = {
-//   PORT: process.env.PORT || 4000,
-//   db: {
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     database: process.env.DB_DATABASE,
-//   },
-// }
+module.exports = {
+  PORT: process.env.PORT || 4000,
+  db: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+  },
+}
